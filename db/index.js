af474cd5e5430189ef6395457e3c2f4e16a3d373
Warren Waterworth <warren.waterworth@gmail.com>
2022-03-25 16:03:36 -0600

    heroku fix

--- db/index.js
+++ db/index.js
@@ -1,6 +1,10 @@
 // Require the Client constructor from the pg package
 const { Client } = require("pg");
 
+ssl: process.env.NODE_ENV === "production"
+  ? { rejectUnauthorized: false }
+  : undefined;
+
 // Create a constant, CONNECTION_STRING, from either process.env.DATABASE_URL or postgres://localhost:5432/phenomena-dev
 const CONNECTION_STRING =
   process.env.DATABASE_URL || "postgres://localhost:5432/phenomena-dev";
